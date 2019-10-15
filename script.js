a7b805b14a0393eee9804a90b6391960731d8dfa
Merelvanpuymbroeck <Merelvanpuymbroeck@gmail.com>
2019-10-15 09:21:43 +0200

    javscript slider

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-$(function(){
-    $('.slideshow img:gt(0)').hide();
-    setInterval(function(){
-      $('.slideshow :first-child').fadeOut()
-         .next('img').fadeIn()
-         .end().appendTo('.slideshow');}, 
-      3000);
-});
+var i = 0;
+var images = [];
+var time = 3000;
+
+images [0] = "Beelden/libelle.jpg";
+images [1] = "Beelden/koppelfoto.jpg";
+images [2] = "Beelden/groepsfoto.jpg";
+
+function changeImg (){
+    document.slide.src = images[i];
+
+    if (i < images.length -1 ) {
+        i++;
+    }
+    else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
